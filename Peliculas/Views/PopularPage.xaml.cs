1753cb751e024d98b031485ea4230b200e869049
Antonio Bedolla <fred-mercury@hotmail.com>
2020-12-22 18:49:26 -0600

    Initial check-in of module Peliculas

---
+++ Peliculas/Views/PopularPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Peliculas.Views
+{
+    public partial class PopularPage : ContentPage
+    {
+        public PopularPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Peliculas/Views/UpcomingPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Peliculas.Views
+{
+    public partial class UpcomingPage : ContentPage
+    {
+        public UpcomingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
